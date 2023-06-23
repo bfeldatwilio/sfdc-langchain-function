f9fcb189da84fdb7cb16f7b5e8f435433f4288eb
bfelda <bfelda@gmail.com>
2023-06-23 12:20:55 -0500

    got it working with apex

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-/**
- * Describe Langchainfunction here.
- *
- * The exported method is the entry point for your code when the function is invoked.
- *
- * Following parameters are pre-configured and provided to your function on execution:
- * @param event: represents the data associated with the occurrence of an event, and
- *                 supporting metadata about the source of that occurrence.
- * @param context: represents the connection to Functions and your Salesforce org.
- * @param logger: logging handler used to capture application logs and trace specifically
- *                 to a given execution of a function.
- */
 export default async function (event, context, logger) {
 
+  const {message} = event.data;
   const orgInfo = {
     id: context.org.id,
     apiVersion: context.org.apiVersion,
     baseUrl: context.org.baseUrl,
-    domainUrl: context.org.domainUrl
+    domainUrl: context.org.domainUrl,
+    message: message
 }
-logger.info(JSON.stringify(orgInfo));
 
-  return orgInfo;
+  return JSON.stringify(orgInfo);
 }
